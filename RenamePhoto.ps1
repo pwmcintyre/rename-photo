6b189f9ba7c829e25c77c8df6d1219612658cb64
Peter McIntyre <peter.mcintyre@gmail.com>
2019-11-10 15:38:06 +1100

    use lightroom style destination path

--- RenamePhoto.ps1
+++ RenamePhoto.ps1
@@ -1,39 +1,26 @@
-# see https://til.secretgeek.net/powershell/rename_photos.html
+# modified from 
+# https://til.secretgeek.net/powershell/rename_photos.html
+# 
 # usage example:
-# dir *.jpg | % { Rename-Photo $_.FullName "iPhoneLB" "Martinique" }
+# ls *.jpg | % { Rename-Photo $_.FullName "iPhoneLB" "Martinique" }
 
-function rename-photo(
-    [ValidateScript({Test-Path $_})][string]$fileName,
-	[String]$device,
-	[String]$location,
-    [int]$AddHours){
+function move-photo(
+	[ValidateScript({Test-Path $_})][string]$source,
+	[ValidateScript({Test-Path $_})][string]$destination
+){
 
-	if ($fileName -eq "") {
-		write-host "Please provide a filename!" -foregroundcolor "red"
-		write-host 'Full example: dir *.jpg | % { Rename-Photo $_.FullName "iPhoneLB" "Martinique" }'
-		return;
+	if ($source -eq "") {
+		write-host "source not found" -foregroundcolor "red"
+		return
 	}
 
-	if ((Test-Path $fileName) -eq $false) {
-		write-host "File not found $fileName!" -foregroundcolor "red"
-		write-host 'Full example: dir *.jpg | % { Rename-Photo $_.FullName "iPhoneLB" "Martinique" }'
-		return;
-	}
-
-	if ($device -eq "") {
-		write-host "Please provide a device name, e.g. iPhoneLB!" -foregroundcolor "red"
-		write-host 'Full example: dir *.jpg | % { Rename-Photo $_.FullName "iPhoneLB" "Martinique" }'
-		return;
-	}
-
-	if ($location -eq "") {
-		write-host "Please provide a location, e.g. Martinique!" -foregroundcolor "red"
-		write-host 'Full example: dir *.jpg | % { Rename-Photo $_.FullName "iPhoneLB" "Martinique" }'
-		return;
+	if ((Test-Path $source) -eq $false) {
+		write-host "source not found: $source" -foregroundcolor "red"
+		return
 	}
 
 	$null = [reflection.assembly]::LoadWithPartialName("System.Drawing")
-	$pic = New-Object System.Drawing.Bitmap($fileName)
+	$pic = New-Object System.Drawing.Bitmap($source)
 
 	# via http://stackoverflow.com/questions/6834259/how-can-i-get-programmatic-access-to-the-date-taken-field-of-an-image-or-video
 	$bitearr = $pic.GetPropertyItem(36867).Value # Date Taken
@@ -45,7 +32,7 @@ function rename-photo(
 		$string = [System.Text.Encoding]::ASCII.GetString($bitearr)
 		$exactDate = [datetime]::ParseExact($string,"yyyy:MM:dd HH:mm:ss`0",$Null)
 
-    } else {
+	} else {
 
 		# we could not extract an EXIF "Date Taken".
 		# perhaps we can one of these dates instead.
@@ -55,18 +42,20 @@ function rename-photo(
 		# LastAccessTimeUtc         Property       datetime LastAccessTimeUtc {get;set;}
 		# LastWriteTime             Property       datetime LastWriteTime {get;set;}
 		# LastWriteTimeUtc          Property       datetime LastWriteTimeUtc {get;set;}
-		dir $fileName | % { $exactDate = $_.LastWriteTime; }
+		dir $source | % { $exactDate = $_.LastWriteTime; }
 	}
 
-	if ($AddHours -ne 0) {
-		$exactDate = $exactDate.AddHours($AddHours)
-	}
+	# build destination path
+	$item = Get-Item $source
+	$newpath = [IO.Path]::Combine( $destination, ("{0:yyyy}" -f $exactDate), ("{0:yyyy-MM-dd}" -f $exactDate), $item.Name )
 
-    $length = (dir $fileName | % length )
-	$extensionWithDot = [io.path]::GetExtension($FileName)
-	$newName = ("{0:yyyy-MM-dd-HH-mm-ss}_{1}_{2}_{3}{4}" -f $exactDate, $device, $location, $length, $extensionWithDot)
+	# troubleshooting
+	# write-host "source: $source"
+	# write-host "exactDate: $exactDate"
+	# write-host ("{0:yyyy}" -f $exactDate)
+	# write-host ("{0:yyyy-MM-dd}" -f $exactDate)
+	write-host $newpath
 
-	write-host "Creating: $newName"
-	rename-item $fileName $newName
+	# rename-item $source $newpath
+
 }
-
