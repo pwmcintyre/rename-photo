59d776f964fdcfbab2f703b9d2dcab5c264f3c90
Peter McIntyre <peter.mcintyre@gmail.com>
2019-11-10 21:40:37 +1100

    refactor

--- RenamePhoto.ps1
+++ RenamePhoto.ps1
@@ -1,61 +1,78 @@
+# Moves files into a path given their lastWriteDate:
+# from: file.ext
+#   to: yyyy/yyyy-MM-dd/file.ext
+# 
 # modified from 
 # https://til.secretgeek.net/powershell/rename_photos.html
 # 
 # usage example:
-# ls *.jpg | % { Rename-Photo $_.FullName "iPhoneLB" "Martinique" }
+# ls *.jpg | Move-Photo -Destination ~/Desktop
 
-function move-photo(
-	[ValidateScript({Test-Path $_})][string]$source,
-	[ValidateScript({Test-Path $_})][string]$destination
+function Move-Photo(
+
+	# ref: https://blog.simonw.se/powershell-functions-and-parameter-sets/
+	[parameter( ParameterSetName = 'Path', Mandatory )]
+	[parameter( ParameterSetName = 'LiteralPath' )]
+	[ValidateScript({Test-Path $_})]
+	[string] $Destination = ".",
+
+	[parameter(
+		Mandatory,
+		ParameterSetName  = 'Path',
+		Position = 0,
+		ValueFromPipeline,
+		ValueFromPipelineByPropertyName
+	)]
+	[ValidateNotNullOrEmpty()]
+	[SupportsWildcards()]
+	[string[]] $Path,
+
+	[parameter(
+		Mandatory,
+		ParameterSetName = 'LiteralPath',
+		Position = 0,
+		ValueFromPipelineByPropertyName
+	)]
+	[ValidateNotNullOrEmpty()]
+	[Alias('PSPath')]
+	[string[]] $LiteralPath
+
 ){
+	BEGIN {
 
-	if ($source -eq "") {
-		write-host "source not found" -foregroundcolor "red"
-		return
+		write-host $Destination
+
 	}
+	PROCESS
+	{
 
-	if ((Test-Path $source) -eq $false) {
-		write-host "source not found: $source" -foregroundcolor "red"
-		return
+		# resolve paths
+		# ref: https://4sysops.com/archives/process-file-paths-from-the-pipeline-in-powershell-functions/
+		if ($PSCmdlet.ParameterSetName -eq 'Path') {
+			$resolvedPaths = Resolve-Path -Path $Path | Select-Object -ExpandProperty Path
+		} elseif ($PSCmdlet.ParameterSetName -eq 'LiteralPath') {
+			$resolvedPaths = Resolve-Path -LiteralPath $LiteralPath | Select-Object -ExpandProperty Path
+		}
+
+		# process
+		foreach ($source in $resolvedPaths) {
+
+			# last write time is good enough
+			$date = ( Get-Item $source ).LastWriteTime
+
+			# build destination path
+			$item = Get-Item $source
+			$newpath = [IO.Path]::Combine( $Destination, ("{0:yyyy}" -f $date), ("{0:yyyy-MM-dd}" -f $date), $item.Name )
+
+			# ensure folder exists
+			mkdir -force (Split-Path $newpath) | Out-Null
+
+			# move
+			# rename-item $source $newpath
+			copy-item $source $newpath -Force
+
+		}
 	}
-
-	$null = [reflection.assembly]::LoadWithPartialName("System.Drawing")
-	$pic = New-Object System.Drawing.Bitmap($source)
-
-	# via http://stackoverflow.com/questions/6834259/how-can-i-get-programmatic-access-to-the-date-taken-field-of-an-image-or-video
-	$bitearr = $pic.GetPropertyItem(36867).Value # Date Taken
-
-	$pic.Dispose()
-
-	if ($bitearr -ne $null) {
-
-		$string = [System.Text.Encoding]::ASCII.GetString($bitearr)
-		$exactDate = [datetime]::ParseExact($string,"yyyy:MM:dd HH:mm:ss`0",$Null)
-
-	} else {
-
-		# we could not extract an EXIF "Date Taken".
-		# perhaps we can one of these dates instead.
-		# CreationTime              Property       datetime CreationTime {get;set;}
-		# CreationTimeUtc           Property       datetime CreationTimeUtc {get;set;}
-		# LastAccessTime            Property       datetime LastAccessTime {get;set;}
-		# LastAccessTimeUtc         Property       datetime LastAccessTimeUtc {get;set;}
-		# LastWriteTime             Property       datetime LastWriteTime {get;set;}
-		# LastWriteTimeUtc          Property       datetime LastWriteTimeUtc {get;set;}
-		dir $source | % { $exactDate = $_.LastWriteTime; }
-	}
-
-	# build destination path
-	$item = Get-Item $source
-	$newpath = [IO.Path]::Combine( $destination, ("{0:yyyy}" -f $exactDate), ("{0:yyyy-MM-dd}" -f $exactDate), $item.Name )
-
-	# troubleshooting
-	# write-host "source: $source"
-	# write-host "exactDate: $exactDate"
-	# write-host ("{0:yyyy}" -f $exactDate)
-	# write-host ("{0:yyyy-MM-dd}" -f $exactDate)
-	write-host $newpath
-
-	# rename-item $source $newpath
+	END {}
 
 }
